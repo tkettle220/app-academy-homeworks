abc1cf2571a336a8d778bc1c563ec5402fb0e814
Thomas Kilmer <tkilmer95@gmail.com>
2017-08-10 20:12:44 -0700

    Complete W2D5 hw

---
+++ W2D5/LRUCache.rb
@@ -0,0 +1,25 @@
+class LRUCache
+
+  def initialize(size)
+    @size = size
+    @cache = []
+  end
+
+  def count
+    @cache.count
+  end
+
+  def add(el)
+    if @cache.include?(el)
+      @cache.delete(el)
+    elsif count >= @size
+      @cache.shift
+    end
+    @cache << el
+  end
+
+  def show
+    p @cache
+  end
+
+end
